e3ca87865134f9b766ba01b57703eea5ba6f4b20
Victor Mateus <victormateus897@gmail.com>
2025-03-10 15:45:14 -0300

    safe backup

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -1,15 +1,20 @@
+import { ConvergiBadge } from "../icons";
 
 export const Portfolio = () => {
   return (
     <div className="bg-[#19204B] flex w-full flex-col items-center px-28 py-24 max-md:px-5 max-md:py-16" style={{ border: '1px solid red' }}>
       <div className="w-full max-w-[1200px]">
-        <div className="flex flex-col mb-12 max-md:mb-8">
+        <div className="flex flex-row justify-between mb-12 max-md:mb-8">
+          <div>
           <div className="text-[#B2B3C7] text-base font-normal">
             Portfolio
           </div>
           <h2 className="text-white text-[64px] font-semibold leading-[72px] mt-6 max-md:text-[40px] max-md:leading-[50px]">
             We Let Our Work<br />Do the Talking
           </h2>
+          </div>
+
+          <ConvergiBadge/>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full text-center">
           <div className="bg-[#2C3158] py-4 px-6 border-b-2 border-[#727EC5]">
