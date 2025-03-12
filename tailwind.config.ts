d958cb278b9a6ed17d5e4c94a56022f85da2ef22
Victor Mateus <victormateus897@gmail.com>
2025-03-12 20:44:25 -0300

    feat: rotate badge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,6 +90,8 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": "spin 3s linear infinite", // Rotação mais lenta (3 segundos)
+        "spin-fast": "spin 500ms linear infinite", // Rotação mais rápida (500ms)
       },
     },
     fontFamily: {
